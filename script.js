7c29e66f78db4709a8b1054d1b4f565f987e2f30
gollivier2022 <guillaume.ollivier2022@campus-eni.fr>
2023-02-28 10:11:53 +0100

    Anonymisation click bouton

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 document.getElementById('date').valueAsDate = new Date();
 document.getElementById('savoir').focus();
+document.getElementById('bouton').addEventListener(
+    click,
+    () => {
+        let nouvelElement = document.createElement('li');
+        let texteSavoir = document.createElement("h2");
+        texteSavoir.innerHTML = document.getElementById("savoir").value;
+        nouvelElement.appendChild(texteSavoir);
+        let texteAuteur = document.createElement("p");
+        texteAuteur.innerHTML = "Par " + document.getElementById("auteur").value + ", le " + document.getElementById("date").value;
+        nouvelElement.appendChild(texteAuteur);
+        nouvelElement.addEventListener("click", supprimerSavoir);
+        document.getElementById("listeSavoir").appendChild(nouvelElement);
+    }
+);
 
-function ajouterSavoir(){
-    let nouvelElement = document.createElement('li');
-    let texteSavoir = document.createElement("h2");
-    texteSavoir.innerHTML = document.getElementById("savoir").value;
-    nouvelElement.appendChild(texteSavoir);
-    let texteAuteur = document.createElement("p");
-    texteAuteur.innerHTML = "Par " + document.getElementById("auteur").value + ", le " + document.getElementById("date").value;
-    nouvelElement.appendChild(texteAuteur);
-    nouvelElement.addEventListener("click", supprimerSavoir);
-    document.getElementById("listeSavoir").appendChild(nouvelElement);
-}
+// function ajouterSavoir(){
+//     let nouvelElement = document.createElement('li');
+//     let texteSavoir = document.createElement("h2");
+//     texteSavoir.innerHTML = document.getElementById("savoir").value;
+//     nouvelElement.appendChild(texteSavoir);
+//     let texteAuteur = document.createElement("p");
+//     texteAuteur.innerHTML = "Par " + document.getElementById("auteur").value + ", le " + document.getElementById("date").value;
+//     nouvelElement.appendChild(texteAuteur);
+//     nouvelElement.addEventListener("click", supprimerSavoir);
+//     document.getElementById("listeSavoir").appendChild(nouvelElement);
+// }
 
 function supprimerSavoir() {
     let savoir = this.firstElementChild.innerText;
-    if (window.confirm("Voulez-vous supprimer le savoir \"" + savoir + "\" ?")){
+    if (window.confirm("Voulez-vous supprimer le savoir \"" + savoir + "\" ?")) {
         this.remove();
     }
 }
