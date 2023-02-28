8fe9ecd4e5fe00e2035ebe035bff9019dbbe17b4
gollivier2022 <guillaume.ollivier2022@campus-eni.fr>
2023-02-28 11:42:25 +0100

    Modif JQuery

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.getElementById('date').valueAsDate = new Date();
 document.getElementById('savoir').focus();
-document.getElementById('bouton').addEventListener(
-    click,
+$("#bouton").click(
     () => {
-        let nouvelElement = document.createElement('li');
-        let texteSavoir = document.createElement("h2");
-        texteSavoir.innerHTML = document.getElementById("savoir").value;
-        nouvelElement.appendChild(texteSavoir);
-        let texteAuteur = document.createElement("p");
-        texteAuteur.innerHTML = "Par " + document.getElementById("auteur").value + ", le " + document.getElementById("date").value;
-        nouvelElement.appendChild(texteAuteur);
-        nouvelElement.addEventListener("click", supprimerSavoir);
-        document.getElementById("listeSavoir").appendChild(nouvelElement);
+        let nouvelElement = $('<li></li>');
+        let texteSavoir = $("<h2></h2>");
+        texteSavoir.html($("#savoir").val());
+        nouvelElement.append(texteSavoir);
+        let texteAuteur = $("<p></p>");
+        texteAuteur.html("Par " + $("#auteur").val() + ", le " + $("#date").val());
+        nouvelElement.append(texteAuteur);
+        nouvelElement.click(supprimerSavoir);
+        $("#listeSavoir").append(nouvelElement);
     }
 );
 
